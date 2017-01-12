35b32a6ac01f5b7569f3cd4cc56eedc4703091b2
Wangolo Joel <wangoloj@outlook.com>
2017-01-12 10:38:39 +0300

    Galois compiled successfuly in the nim language.

--- src/galois.nim
+++ src/galois.nim
@@ -81,20 +81,36 @@ type
       extract_word*: gf_extract
       scratch*: pointer
 
-proc galois_init_default_field*(w: cint): cint
-proc galois_uninit_field*(w: cint): cint
-proc galois_change_technique*(gf: ptr gf_t; w: cint)
-proc galois_single_multiply*(a: cint; b: cint; w: cint): cint
-proc galois_single_divide*(a: cint; b: cint; w: cint): cint
-proc galois_inverse*(x: cint; w: cint): cint
-proc galois_region_xor*(src: cstring; dest: cstring; nbytes: cint)
+proc galois_init_default_field*(w: cint): cint =
+    echo "Galois init default field"
+
+proc galois_uninit_field*(w: cint): cint =
+    echo "Galois uninit field"
+
+proc galois_change_technique*(gf: ptr gf_t; w: cint): cint =
+    echo "Galious change technique"
+
+proc galois_single_multiply*(a: cint; b: cint; w: cint): cint =
+    echo "Galoius singl multiply"
+
+proc galois_single_divide*(a: cint; b: cint; w: cint): cint =
+    echo "Galious single divide"
+
+proc galois_inverse*(x: cint; w: cint): cint =
+   echo "Galois inverse"
+
+proc galois_region_xor*(src: cstring; dest: cstring; nbytes: cint): cint =
+   echo "Galois region"
+
   ##  Source Region
   ##  Dest Region (holds result)
 ##  Number of bytes in region
 ##  These multiply regions in w=8, w=16 and w=32.  They are much faster
 ##    than calling galois_single_multiply.  The regions must be long word aligned.
 
-proc galois_w08_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2: cstring; add: cint)
+proc galois_w08_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2: cstring; add: cint): cint =
+  echo "Making this work"
+
   ##  Region to multiply
   ##  Number to multiply by
   ##  Number of bytes in region
@@ -102,7 +118,8 @@ proc galois_w08_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2
   ##                                                        Otherwise region is overwritten
 ##  If (r2 != NULL && add) the produce is XOR'd with r2
 
-proc galois_w16_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2: cstring; add: cint)
+proc galois_w16_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2: cstring; add: cint): cint =
+    echo "Galois w16 region multiply"
   ##  Region to multiply
   ##  Number to multiply by
   ##  Number of bytes in region
@@ -110,7 +127,9 @@ proc galois_w16_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2
   ##                                                        Otherwise region is overwritten
 ##  If (r2 != NULL && add) the produce is XOR'd with r2
 
-proc galois_w32_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2: cstring; add: cint)
+proc galois_w32_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2: cstring; add: cint): cint =
+   echo "galois_w32_region_multiply"
+
   ##  Region to multiply
   ##  Number to multiply by
   ##  Number of bytes in region
@@ -118,6 +137,12 @@ proc galois_w32_region_multiply*(region: cstring; multby: cint; nbytes: cint; r2
   ##                                                        Otherwise region is overwritten
 ##  If (r2 != NULL && add) the produce is XOR'd with r2
 
-proc galois_init_field*(w: cint; mult_type: cint; region_type: cint; divide_type: cint; prim_poly: uint64; arg1: cint; arg2: cint): ptr gf_t
-proc galois_init_composite_field*(w: cint; region_type: cint; divide_type: cint; degree: cint; base_gf: ptr gf_t): ptr gf_t
-proc galois_get_field_ptr*(w: cint): ptr gf_t 
+proc galois_init_field*(w: cint; mult_type: cint; region_type: cint; divide_type: cint; prim_poly: uint64; arg1: cint; arg2: cint): ptr gf_t =
+    echo "Galois init field"
+
+proc galois_init_composite_field*(w: cint; region_type: cint; divide_type: cint; degree: cint; base_gf: ptr gf_t): ptr gf_t =
+    echo "Galois int composite field"
+
+proc galois_get_field_ptr*(w: cint): ptr gf_t =
+    echo "Galois get field ptr"
+
