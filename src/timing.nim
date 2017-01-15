8a877cc6527042e7f558cab8b51c41e707a0d909
Wangolo Joel <wangoloj@mail.com>
2017-01-15 19:16:50 +0300

    Manually modified another nim-lang transted c header reed_sol.nim, compiled it and it worked very fine!

--- src/timing.nim
+++ src/timing.nim
@@ -7,7 +7,8 @@ const
 
 when defined(USE_CLOCK):
 else:
-## 
+
+##
 ## struct timing {
 ## #ifdef USE_CLOCK
 ##   clock_t clock;
@@ -15,17 +16,24 @@ else:
 ##   struct timeval tv;
 ## #endif
 ## };
-## 
+##
 ##  Get the current time as a double in seconds.
 
-proc timing_now*(): cdouble {.cdecl, importc: "timing_now", dynlib: libname.}
-##  Set *t to the current time.
+proc timing_now*(): cdouble
+  #{.cdecl, importc: "timing_now", dynlib: libname.}
+  ##  Set *t to the current time.
+  echo "We are under timing_now"
 
-proc timing_set*(t: ptr timing) {.cdecl, importc: "timing_set", dynlib: libname.}
-##  Get *t as a double in seconds.
+proc timing_set*(t: ptr timing): ptr
+  #{.cdecl, importc: "timing_set", dynlib: libname.}
+  ##  Get *t as a double in seconds.
+  echo "We are under timme_set"
 
-proc timing_get*(t: ptr timing): cdouble {.cdecl, importc: "timing_get", dynlib: libname.}
-##  Return *t2 - *t1 as a double in seconds.
+proc timing_get*(t: ptr timing): cdouble
+  #{.cdecl, importc: "timing_get", dynlib: libname.}
+  ##  Return *t2 - *t1 as a double in seconds.
+  echo "We are under timing_get"
 
-proc timing_delta*(t1: ptr timing; t2: ptr timing): cdouble {.cdecl,
-    importc: "timing_delta", dynlib: libname.}
+proc timing_delta*(t1: ptr timing; t2: ptr timing): cdouble
+  #{.cdecl,importc: "timing_delta", dynlib: libname.}
+    echo "We are under timing delta"
