36127818044fadbb3be05e7149b2037117ea3558
Wangolo Joel <wangoloj@mail.com>
2017-01-20 22:11:44 +0300

    Manaully modified the timing.nim, the manaul configurations worked after compilations.

--- src/timing.nim
+++ src/timing.nim
@@ -2,11 +2,21 @@
 
 ##  Define USE_CLOCK to use clock(). Otherwise use gettimeofday().
 
-const
-  USE_CLOCK* = true
+## NOTE: Wangolo Joel (wangoloj@outlook.com)
+## In the corresponding C library there seemed to be a choice
+## to the developers to use the <time.h> and the <sys/time.h>
+## That is why they deciced to include #define USE_CLOCK
+## And it looks like we here in the nim-lang we have only one choice time
+## We may decide to comment off this line of code entirely and just
+## import the time module.
 
-when defined(USE_CLOCK):
-else:
+
+## const
+##   USE_CLOCK* = true
+
+## when defined(USE_CLOCK):
+## else:
+## Read the commend about on the NOTE:----------------------
 
 ##
 ## struct timing {
@@ -18,22 +28,26 @@ else:
 ## };
 ##
 ##  Get the current time as a double in seconds.
+import times
 
-proc timing_now*(): cdouble
+type
+    timing* = object
+
+proc timing_now*(): cdouble =
   #{.cdecl, importc: "timing_now", dynlib: libname.}
   ##  Set *t to the current time.
   echo "We are under timing_now"
 
-proc timing_set*(t: ptr timing): ptr
+proc timing_set*(t: ptr timing): ptr =
   #{.cdecl, importc: "timing_set", dynlib: libname.}
   ##  Get *t as a double in seconds.
   echo "We are under timme_set"
 
-proc timing_get*(t: ptr timing): cdouble
+proc timing_get*(t: ptr timing): cdouble =
   #{.cdecl, importc: "timing_get", dynlib: libname.}
   ##  Return *t2 - *t1 as a double in seconds.
   echo "We are under timing_get"
 
-proc timing_delta*(t1: ptr timing; t2: ptr timing): cdouble
+proc timing_delta*(t1: ptr timing; t2: ptr timing): cdouble =
   #{.cdecl,importc: "timing_delta", dynlib: libname.}
     echo "We are under timing delta"
