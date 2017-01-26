4b09d066f9c7994a3b8d50ef19f37114a7e8dd7e
Wangolo Joel <wangoloj@mail.com>
2017-01-26 17:10:25 +0300

    New galois changes

--- src/galois.nim
+++ src/galois.nim
@@ -37,6 +37,43 @@
 ##  POSSIBILITY OF SUCH DAMAGE.
 ##
 
+var gf_errno*: cint
+
+type
+  GFP* = ptr object ## gf
+
+type
+  ##GFP* = ptr gf
+  gf_func_a_b* = object {.union.}
+    w32*: proc (gf: GFP; a: gf_val_32_t; b: gf_val_32_t): gf_val_32_t
+    w64*: proc (gf: GFP; a: gf_val_64_t; b: gf_val_64_t): gf_val_64_t
+    w128*: proc (gf: GFP; a: gf_val_128_t; b: gf_val_128_t; c: gf_val_128_t)
+
+  gf_func_a* = object {.union.}
+    w32*: proc (gf: GFP; a: gf_val_32_t): gf_val_32_t
+    w64*: proc (gf: GFP; a: gf_val_64_t): gf_val_64_t
+    w128*: proc (gf: GFP; a: gf_val_128_t; b: gf_val_128_t)
+
+  gf_region* = object {.union.}
+    w32*: proc (gf: GFP; src: pointer; dest: pointer; val: gf_val_32_t; bytes: cint;
+              add: cint)
+    w64*: proc (gf: GFP; src: pointer; dest: pointer; val: gf_val_64_t; bytes: cint;
+              add: cint)
+    w128*: proc (gf: GFP; src: pointer; dest: pointer; val: gf_val_128_t; bytes: cint;
+               add: cint)
+
+  gf_extract* = object {.union.}
+    w32*: proc (gf: GFP; start: pointer; bytes: cint; index: cint): gf_val_32_t
+    w64*: proc (gf: GFP; start: pointer; bytes: cint; index: cint): gf_val_64_t
+    w128*: proc (gf: GFP; start: pointer; bytes: cint; index: cint; rv: gf_val_128_t)
+
+  gf_t* = object
+    multiply*: gf_func_a_b
+    divide*: gf_func_a_b
+    inverse*: gf_func_a
+    multiply_region*: gf_region
+    extract_word*: gf_extract
+    scratch*: pointer
 
 
 const
