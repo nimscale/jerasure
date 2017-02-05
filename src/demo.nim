dbfd18e29c0fe135c21c04df955b7a8efe639bb8
Wangolo Joel <wangoloj@mail.com>
2017-02-05 19:53:55 +0300

    Attempting to translating depending headers gf_cpu

--- src/demo.nim
+++ src/demo.nim
@@ -3,4 +3,30 @@ import galois
 
 var tmp: cint = 12
 
-echo galois_init_default_field(tmp)
+#echo galois_init_default_field(tmp)
+
+#echo hostCPU
+
+var arc_x86: bool = false
+var arc_x64: bool = false
+var arc_xUnknown: bool = false
+var arc_x86_x64: bool = false
+
+if hostCPU == "amd64":
+  arc_x64 = true
+
+elif hostCPU == "i386":
+  arc_x86 = true
+else:
+  arc_xUnknown = true
+
+if arc_x64 or arc_x86:
+   const
+      arc_x86_64* =true
+
+else:
+   echo "There is no hope for the arch to work@"
+
+when defined(arc_x86_64):
+    echo "it's a welcome!"
+   
--- src/gf_cpu.nim
+++ src/gf_cpu.nim
@@ -17,3 +17,30 @@ var gf_cpu_supports_intel_ssse3*: cint
 var gf_cpu_supports_intel_sse3*: cint
 var gf_cpu_supports_intel_sse2*: cint
 var gf_cpu_supports_arm_neon*: cint
+
+# Did not find any equivalent to C __x86_x64__ in nimlang, but
+# Decided alittle bit to create a simpple way of definiing this marcors
+var arc_x86: bool = false
+var arc_x64: bool = false
+var arc_xUnknown: bool = false
+
+
+if hostCPU == "amd64":
+  arc_x64 = true
+
+elif hostCPU == "i386":
+  arc_x86 = true
+else:
+  arc_xUnknown = true
+
+when defined(arc_x64):
+
+#if arc_x64 or arc_x86:
+  ##  CPUID Feature Bits
+  ##  ECX
+  const
+    GF_CPU_SSE3* = (1 shl 0)
+    GF_CPU_PCLMUL* = (1 shl 1)
+    GF_CPU_SSSE3* = (1 shl 9)
+    GF_CPU_SSE41* = (1 shl 19)
+    GF_CPU_SSE42* = (1 shl 20)
